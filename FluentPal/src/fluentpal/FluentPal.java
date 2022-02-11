40d29f3f716296c9f41992a895cb684e8439ccaa
AlainEkh <alain.ekmekdjian@outlook.com>
2022-02-11 15:08:32 -0500

    Revert "Blank main"
    This reverts commit 1a6848d39e1ec92b5cf65eba2bc1a03aee6d331b.

--- FluentPal/src/fluentpal/FluentPal.java
+++ FluentPal/src/fluentpal/FluentPal.java
@@ -6,7 +6,8 @@ public class FluentPal {
 
    
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
     
 }
