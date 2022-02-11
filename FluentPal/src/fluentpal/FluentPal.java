be1af1fbe76b64f4ef3caf08d9ebf186ea54153e
AlainEkh <alain.ekmekdjian@outlook.com>
2022-02-11 15:08:52 -0500

    Blank main

--- FluentPal/src/fluentpal/FluentPal.java
+++ FluentPal/src/fluentpal/FluentPal.java
@@ -6,8 +6,7 @@ public class FluentPal {
 
    
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World2");
+        
     }
     
 }
